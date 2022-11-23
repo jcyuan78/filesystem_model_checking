8cacdcc01f0482ab17411f9aa0cdb101112c2ddf
Jingcheng Yuan <bytekiller1978@hotmail.com>
2022-11-23 15:09:20 +0900

    Initial add driver package

---
+++ application/device-driver/driver-package/source/dllmain.cpp
@@ -0,0 +1,68 @@
+﻿// dllmain.cpp : 定义 DLL 应用程序的入口点。
+#include "pch.h"
+
+#include <dokanfs-lib.h>
+#include <include\journal_device.h>
+#include <include/file_disk.h>
+#include <boost/property_tree/ptree.hpp>
+
+BOOL APIENTRY DllMain(HMODULE hModule, DWORD  ul_reason_for_call, LPVOID lpReserved)
+{
+    switch (ul_reason_for_call)
+    {
+    case DLL_PROCESS_ATTACH:
+    case DLL_THREAD_ATTACH:
+    case DLL_THREAD_DETACH:
+    case DLL_PROCESS_DETACH:
+        break;
+    }
+    return TRUE;
+};
+
+
+class CDeviceFactory : public IFsFactory
+{
+public:
+    virtual bool CreateFileSystem(IFileSystem*& fs, const std::wstring& fs_name) { return false; }
+    //virtual bool CreateVirtualDisk(IVirtualDisk * & dev, const std::wstring & fn, size_t secs);
+    virtual bool CreateVirtualDisk(IVirtualDisk*& dev, const boost::property_tree::wptree& prop, bool create)
+    {
+        const std::wstring& dev_name = prop.get<std::wstring>(L"name", L"");
+        if (dev_name == L"journal")
+        {
+            CJournalDevice* _dev = jcvos::CDynamicInstance<CJournalDevice>::Create();
+            if (_dev == NULL) THROW_ERROR(ERR_MEM, L"failed on creating CJournalDevice");
+            bool br = _dev->InitializeDevice(prop);
+            if (!br)
+            {
+                _dev->Release();
+                THROW_ERROR(ERR_APP, L"failed on config Journal Device");
+            }
+            dev = static_cast<IVirtualDisk*>(_dev);
+        }
+        else if (dev_name == L"file_disk")
+        {
+            CFileDisk* _dev = jcvos::CDynamicInstance<CFileDisk>::Create();
+            if (_dev == nullptr) THROW_ERROR(ERR_MEM, L"failed on creating CFileDisk");
+            bool br = _dev->InitializeDevice(prop);
+            if (!br)
+            {
+                _dev->Release();
+                THROW_ERROR(ERR_APP, L"failed on config File Disk");
+            }
+            dev = static_cast<IVirtualDisk*>(_dev);
+        }
+        else { return false; }
+        return true;
+    }
+
+};
+
+jcvos::CStaticInstance<CDeviceFactory> g_factory;
+
+extern "C" __declspec(dllexport) bool GetFactory(IFsFactory * &fac)
+{
+    JCASSERT(fac == NULL);
+    fac = static_cast<IFsFactory*>(&g_factory);
+    return true;
+}
---
+++ application/device-driver/driver-package/source/pch.h
@@ -0,0 +1,30 @@
+﻿// pch.h: 这是预编译标头文件。
+// 下方列出的文件仅编译一次，提高了将来生成的生成性能。
+// 这还将影响 IntelliSense 性能，包括代码完成和许多代码浏览功能。
+// 但是，如果此处列出的文件中的任何一个在生成之间有更新，它们全部都将被重新编译。
+// 请勿在此处添加要频繁更新的文件，这将使得性能优势无效。
+
+#ifndef PCH_H
+#define PCH_H
+
+#define WIN32_NO_STATUS
+
+// 添加要在此处预编译的标头
+#include "framework.h"
+
+#include <stdio.h>
+#include <tchar.h>
+
+
+//#define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers
+// Windows Header Files
+
+//#include <windows.h>
+#define LOG_OUT_CLASS_SIZE
+#define LOGGER_LEVEL LOGGER_LEVEL_DEBUGINFO
+
+#include <stdext.h>
+
+#undef WIN32_NO_STATUS
+
+#endif //PCH_H
